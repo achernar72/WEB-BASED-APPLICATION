2f9279d7fb9ed22d783001fc7c8776d23706138c
achernar72 <sankarasettyavinash@gmail.com>
2017-07-06 21:48:19 -0500

    the files are added

--- Assignment3/Source/GitHubFinder/script.js
+++ Assignment3/Source/GitHubFinder/script.js
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+/**
+ * Created by avinash on 6/28/2017.
+ */
+function check(form) {
+    /*when the username avi and password 12345 will match it will redirects the new page*/
+
+    if(form.user.value == "avi" && form.password.value == "12345") {
+        window.open('git.html')/*here where it will redirects toi the next page and continues*/
+    }
+    else {
+        alert("Entered Password or Username doesn't match")/*where it will shows the entered password and username shouldnt match it will shows the error*/
+    }
+}
+
+function githubfinder(){
+
+
+
+}
+=======
 /**
  * Created by nagar on 6/28/2017.
  */
+>>>>>>> f43658de4464ca527794a44c86933a8dcc81304f
