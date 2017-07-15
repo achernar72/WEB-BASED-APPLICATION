16287e084d525734c1a26d6edcaf5fbce858877b
achernar72 <sankarasettyavinash@gmail.com>
2017-07-15 15:55:19 -0500

    the screen sots are added

--- Assignment5/Source/geolocation/script.js
+++ Assignment5/Source/geolocation/script.js
@@ -1,48 +1,70 @@
-/**
- * Created by ash on 7/13/17.
- */
-var APPID ="e8af413c6d27ca7824587e11c7af0ef2";
-var temp;
-var locate;
-function updateByZip1(zip){
-    var url="http://api.openweathermap.org/data/2.5/weather?"+
-        "zip=" +zip+
-        "&APPID=" +APPID;
-    sendRequest(url);
-}
-function updateByZip2(zip){
-    var url="http://api.openweathermap.org/data/2.5/weather?"+
-        "zip=" +zip+
-        "&APPID=" +APPID;
-    sendRequest(url);
-}
-function updateByCity(City){
-    var url="http://api.openweathermap.org/data/2.5/weather?q="+
-        "city"+city+
-        "&APPID=" +APPID;
-    sendRequest(url);
-}
-function sendRequest(url){
-    var xmlhttp =new XMLHttpRequest();
-    xmlhttp.onreadystatechange= function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            var data =JSON.parse(xmlhttp.responseText);
-            var weather={};
-            weather.locate=data.name;
-            weather.temp=data.main.temp;
-            update(weather);
-        }
+var app=angular.module('GoogleDirection', []);
+
+app.controller('googlemaps', function ($scope) {
+    var map;
+    var mapOptions;
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+        draggable: true
+    });
+    var directionsService = new google.maps.DirectionsService();
+    $scope.initialize = function () {
+        var pos = new google.maps.LatLng(0, 0);
+        var mapOptions = {
+            zoom: 3,
+            center: pos
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
     };
-    xmlhttp.open("GET",url,true);
-    xmlhttp.send();
-}
-function update(weather){
-    temp.innerHTML=weather.temp;
-    locate.innerHTML=weather.locate;
-}
-window.onload=function(){
-    temp=document.getElementById("temperature");
-    locate=document.getElementById("location");
-    updateByZip1("64110");
-    updateByZip2("64116");
-}
+    $scope.calcRoute = function () {
+        var end = document.getElementById('des_loc').value;
+        var start = document.getElementById('source_loc').value;
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setMap(map);
+                directionsDisplay.setDirections(response);
+                console.log(status);
+            }
+
+        });
+    };
+    google.maps.event.addDomListener(window, 'load', $scope.initialize);
+});
+
+
+app.controller('weatherCtrl', ['$scope', 'weatherService', function($scope, weatherService) {
+    function fetchWeather(zip) {
+        weatherService.getWeather(zip).then(function(data){
+            $scope.place = data;
+        });
+    }
+    fetchWeather('Kansas City');
+    $scope.findWeather = function(zip) {
+        $scope.place = '';
+        loc=document.getElementById('source_loc').value;
+        fetchWeather(loc);
+    };
+}]);
+
+app.factory('weatherService', ['$http', '$q', function ($http, $q){
+    function getWeather (zip) {
+        var deferred = $q.defer();
+        $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ zip +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+            .success(function(data){
+                deferred.resolve(data.query.results.channel);
+            })
+            .error(function(err){
+                console.log('Error retrieving markets');
+                deferred.reject(err);
+            });
+        return deferred.promise;
+    }
+    return {
+        getWeather: getWeather
+    };
+}]);
